4701643ae89bc6bbcd81716093843979a130ee7e
Joseph Senders <jasenders@gmail.com>
2018-06-07 00:35:25 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
 def countdown(n)
-n = 12
-  while n > 0
-      n -= 12
+counter = 12
+  while counter > 0
+      counter -= 12
     puts "#{n} SECOND(S)!"
   end
   return "HAPPY NEW YEAR!"
